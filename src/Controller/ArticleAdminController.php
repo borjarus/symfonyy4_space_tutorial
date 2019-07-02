4c661a3a8d65f4832d372d0ce6d807402d099902
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-07-02 19:58:02 +0200

    IS_AUTHENTICATED_ & Protecting All URLs

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @IsGranted("ROLE_ADMIN")
