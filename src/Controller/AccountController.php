2d863a1a5964f76573692440365f7cdd5f24e03a
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-07-11 07:17:36 +0200

    Serializer & API Endpoint

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -20,4 +20,17 @@ class AccountController extends BaseController
                 
         ]);
     }
+
+    /**
+     *
+     * @Route("/api/account", name="api_account")
+     */
+    public function accountApi()
+    {
+        $user = $this->getUser();
+
+        return $this->json($user,200,[], [
+            'groups' => ['main']
+        ]);
+    }
 }
