3cad649e4f2b6089d008f9e466b5dc397dc98f44
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-08-06 09:17:52 +0200

    HTML5 & "Sanity" Validation

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -31,7 +31,8 @@ class ArticleFormType extends AbstractType
                     return sprintf('(%d) %s', $user->getId(), $user->getEmail());
                 },
                 'placeholder' => 'Choose the author',
-                'choices' => $this->userRepository->findAllEmailAlphabetical()
+                'choices' => $this->userRepository->findAllEmailAlphabetical(),
+                'invalid_message' => 'Symfony is too smart for your hacking!'
             ])
         ;
         // [
