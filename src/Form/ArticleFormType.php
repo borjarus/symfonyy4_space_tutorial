04852b5a74fac4d82d1fc58c6a40adfa27608ea6
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-08-06 08:58:39 +0200

    EntityType: Custom Query

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -1,12 +1,22 @@
 <?php
 namespace App\Form;
+use App\Entity\User;
 use App\Entity\Article;
+use App\Repository\UserRepository;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class ArticleFormType extends AbstractType
 {
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -14,6 +24,14 @@ class ArticleFormType extends AbstractType
             ->add('content')
             ->add('publishedAt', null, [
                 'widget' => 'single_text'
+            ])
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => function(User $user) {
+                    return sprintf('(%d) %s', $user->getId(), $user->getEmail());
+                },
+                'placeholder' => 'Choose the author',
+                'choices' => $this->userRepository->findAllEmailAlphabetical()
             ])
         ;
         // [
